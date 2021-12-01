5f39228f2274e19b8954382c202446aaa9e8c69d
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2021-12-01 18:05:52 +0200

    set up basic routing

---
+++ Real/src/pages/AllQuotes.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AllQuotes = () => {
+  return <h1>AllQuotes Page</h1>;
+};
+
+export default AllQuotes;
---
+++ Real/src/pages/NewQuote.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewQuote = () => {
+  return <h1>NewQuote Page</h1>;
+};
+
+export default NewQuote;
