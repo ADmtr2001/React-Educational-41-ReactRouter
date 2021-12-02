6f25d4f01e65c719d83fddcbd7eecb997074bd02
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2021-12-02 15:53:35 +0200

    add useRouteMatch

--- Real/src/pages/QuoteDetail.js
+++ Real/src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, useParams } from "react-router";
+import { Route, useParams, useRouteMatch } from "react-router";
 import { Fragment } from "react/cjs/react.production.min";
 
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
@@ -12,6 +12,8 @@ const DUMMY_QUOTES = [
 ];
 
 const QuoteDetail = () => {
+  const match = useRouteMatch();
+  console.log(match);
   const params = useParams();
 
   const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
@@ -23,14 +25,14 @@ const QuoteDetail = () => {
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
-      <Route path={`/quotes/${params.quoteId}`} exact>
+      <Route path={`${match.path}`} exact>
         <div className='centered'>
-          <Link className='btn--flat' to={`/quotes/${params.quoteId}/comments`}>
+          <Link className='btn--flat' to={`${match.url}/comments`}>
             Load Comments
           </Link>
         </div>
       </Route>
-      <Route path={`/quotes/${params.quoteId}/comments`}>
+      <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
     </Fragment>
